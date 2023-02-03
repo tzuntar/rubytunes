ab14827b9562893ca2f544bce7b484d401ad376b
Tobija Žuntar <tobija.zuntar@gmail.com>
2023-02-03 17:59:19 +0100

    Enable posting new comments on song pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :albums
   resources :artists
-  resources :songs
+  resources :songs do
+    resources :comments, only: [:create, :update, :destroy]
+  end
   #get '/song/upload', to: 'songs#upload', as: 'song_upload'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
