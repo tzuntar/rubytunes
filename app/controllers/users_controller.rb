126b6a5e29328c9f598bedb051834e14be1793e5
Tobija Žuntar <tobija.zuntar@gmail.com>
2023-02-14 18:47:16 +0100

    User Preferences front-end (changing the password and the profile picture)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,6 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   #before_action :authorize_user
 
-  # GET /user/preferences
-  def preferences
-    @user = current_user
-  end
-
   private
 
   # Use callbacks to share common setup or constraints between actions.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_scope :user  do
+    get '/user/preferences' => 'devise/registrations#edit', as: 'user_preferences'
+  end
   devise_for :users
   resources :albums
   resources :artists
@@ -6,7 +9,6 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :update, :destroy]
   end
   get '/user/:user_id/songs', to: 'songs#index_by_user'
-  get '/user/preferences', to: 'users#preferences', as: 'user_preferences'
   get '/search', to: 'songs#search', as: 'search'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
