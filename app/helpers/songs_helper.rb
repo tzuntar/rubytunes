041d7a10b0822ee2e84eddaf1518a452fefb89aa
Tobija Žuntar <tobija.zuntar@gmail.com>
2023-02-16 09:18:01 +0100

    Add Edit and Delete buttons to the Song page

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,2 +1,19 @@
 module SongsHelper
+
+  def edit_button
+    link_to edit_song_url do
+      button_tag class: 'play-button' do
+        image_tag 'icons/edit', class: 'icon-make-smaller', alt: 'Edit'
+      end
+    end
+  end
+
+  def delete_button
+    link_to song_url, data: { confirm: 'Are you sure you would like to delete this song? This action cannot be undone' }, method: :delete do
+      button_tag class: 'play-button' do
+        image_tag 'icons/delete', class: 'icon-make-smaller', alt: 'Delete'
+      end
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,13 @@ class User < ApplicationRecord
   has_many :songs, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_one_attached :profile_pic
+
+  def profile_pic_thumbnail
+    if profile_pic.attached?
+      #profile_pic.variant(resize: '150x150!').processed
+      profile_pic
+    else
+      'icons/avatar'
+    end
+  end
 end
